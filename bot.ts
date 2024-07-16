eb45688c4647b44cb0cddd26ab2571f8e6495a73
rexowr <alishmast23@gmail.com>
2024-07-16 16:30:54 +0330

    updated codes

--- bot.ts
+++ bot.ts
@@ -1,10 +1,11 @@
-import { Api, Bot, Context, GrammyError, HttpError, type RawApi } from "grammy"
-import { backMenu, backPanel, indexMenu, panel } from "./src/keyboard"
+import {Api, Bot, Context, GrammyError, HttpError, type RawApi} from "grammy"
+import {backMenu, backPanel, indexMenu, panel} from "./src/keyboard"
 import connect from "./src/connectToDatabase"
 import User from "./src/models/User"
-import { checkQueue, generateImage } from "./src"
+import {checkQueue, generateImage} from "./src"
 
 const token: string = "6870206997:AAEyiWpIwS418SjqzLykz4VYoEtd97sCUb8"
+
 interface BotConfig {
   botDeveloper: number;
   isDeveloper: boolean;
@@ -39,35 +40,56 @@ bot.command("start", async ctx => {
   await ctx.reply(`سلام ${firstName} عزیز\nبه ربات پرشین میدجرنی خوش اومدی\nاز طریق ربات میتونی کلی عکسای جذاب با هوش مصنوعی بسازی اونم با کیفیت بالا!\nبرای شروع از دکمه های زیر استفاده کن`, {
     reply_markup: indexMenu
   })
-  const exists = await User.findOne({ userId })
+  const exists = await User.findOne({userId})
   if (!exists) {
-    await User.create({ userId, firstName, username, charge: 5 })
+    await User.create({userId, firstName, username, charge: 5})
     await ctx.reply("به شما 5 کریت رایگان برای ساخت عکس هدیه داده شد🎉")
   }
 })
 
+let stats: { to?: string | number } = {to: 1}
 bot.on(":text", async ctx => {
   const userId = ctx.from?.id
-  const user = await User.findOne({ userId })
-  console.log("from text: ", user)
-  if(user?.step === "send_prompt"){
+  const user = await User.findOne({userId})
+  const msg = ctx.msg.text
+  if (user?.step === "send_charge") {
+    const toUser = ctx.msg.text
+    status.to = toUser
+    const userExists = await User.findOne({userId: toUser})
+    if (userExists) {
+      await ctx.reply("لطفا مقدار کریت را ارسال کنید")
+      await User.findOneAndUpdate({userId}, {step: "send_credit"})
+    } else {
+      await ctx.reply("کاربر در دیتابیس وجود ندارد")
+      await User.findOneAndUpdate({userId}, {step: "null"})
+    }
+  }
+  if (user?.step === "send_credit") {
+    const amount = ctx.msg.text
+    await User.findOneAndUpdate({userId: stats.to}, {charge: amount})
+    await ctx.reply("حساب مورد نظر شارژ شد")
+    await User.findOneAndUpdate({userId}, {step: "null"})
+  }
+  if (user?.step === "send_prompt") {
     const prompt = ctx.msg.text
-    if(user.charge > 0){
+    if (user.charge > 0) {
       await ctx.deleteMessages([ctx.msg.message_id - 1])
       const result = await generateImage(prompt)
       await ctx.reply(`درخواست شما با ایدی ${result.uid} وارد صف انتظار شد\nعکس به محض ساخته شدن ارسال خواهد شد`)
       const check = await checkQueue(result.uid)
       if (check) await ctx.replyWithPhoto(check.image.url)
-      await User.findOneAndUpdate({ userId }, { charge: user.charge - 1, step: "null" })
+      await User.findOneAndUpdate({userId}, {charge: user.charge - 1, step: "null"})
     } else {
       await ctx.reply("موجودی شما کافی نیست")
-      await User.findOneAndUpdate({ userId }, { step: "null" })
+      await User.findOneAndUpdate({userId}, {step: "null"})
     }
   }
 })
 
 bot.command("panel", async ctx => {
-  if(ctx.config.isDeveloper){}
+  if (ctx.config.isDeveloper) {
+    await ctx.reply("admin panel", {reply_markup: panel})
+  }
 })
 
 bot.catch((err) => {
--- src/keyboard/index.ts
+++ src/keyboard/index.ts
@@ -1,4 +1,4 @@
-import { Menu } from "@grammyjs/menu"
+import {Menu} from "@grammyjs/menu"
 import User from "../models/User"
 
 const indexMenu = new Menu("index-menu")
@@ -7,13 +7,13 @@ const indexMenu = new Menu("index-menu")
     await ctx.editMessageText("لطفا متن خود را ارسال کنید:", {
       reply_markup: backMenu
     })
-    await User.findOneAndUpdate({ userId }, { step: "send_prompt" })
+    await User.findOneAndUpdate({userId}, {step: "send_prompt"})
   })
   .row()
   // .submenu("حساب کاربری👤", "credits-menu")
   .text("حساب کاربری👤", async ctx => {
     const userId = ctx.from.id
-    const user = await User.findOne({ userId })
+    const user = await User.findOne({userId})
     await ctx.editMessageText(`👨🏻مشخصات حساب شما:\nایدی: ${user?.userId}\nنام: ${user?.firstName}\nتعداد کریت: ${user?.charge}\nتاریخ عضویت: ${user?.dateJoined.toLocaleDateString("fa")}`, {
       reply_markup: backMenu
     })
@@ -30,14 +30,14 @@ const backMenu = new Menu("back-menu")
   .back("بازگشت", async ctx => {
     const firstName = ctx.from?.first_name
     const userId = ctx.from.id
-    await User.findOneAndUpdate({ userId }, { step: "null" })
+    await User.findOneAndUpdate({userId}, {step: "null"})
     await ctx.editMessageText(`سلام ${firstName} عزیز\nبه ربات پرشین میدجرنی خوش اومدی\nاز طریق ربات میتونی کلی عکسای جذاب با هوش مصنوعی بسازی اونم با کیفیت بالا!\nبرای شروع از دکمه های زیر استفاده کن`)
   })
 
 const backPanel = new Menu("back-panel")
   .back("بازگشت", async ctx => {
     const userId = ctx.from.id
-    await User.findOneAndUpdate({ userId }, { step: "null" })
+    await User.findOneAndUpdate({userId}, {step: "null"})
     await ctx.editMessageText("به پنل مدیریت خوش اومدین")
   })
 
@@ -47,11 +47,12 @@ const panel = new Menu("panel-admin")
     await ctx.editMessageText("لطفا ایدی عددی کاربر موردنظر را ارسال کنید", {
       reply_markup: backPanel
     })
+    await User.findOneAndUpdate({userId}, {step: "send_charge"})
   })
   .row()
   .text("امار کاربران")
   .row()
-  .text("بستن پنل")
+  .text("بستن پنل", async ctx => await ctx.deleteMessage())
 
 export {
   indexMenu,
